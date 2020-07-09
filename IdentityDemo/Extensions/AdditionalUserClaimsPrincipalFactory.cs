335a1220b4eec7ea996a063f1eb2029a72ce330d
akrassikov <akrassikov@angus-systems.com>
2020-07-09 11:17:35 -0400

    Add project files.

---
+++ IdentityDemo/Extensions/AdditionalUserClaimsPrincipalFactory.cs
@@ -0,0 +1,46 @@
+﻿using IdentityDemo.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Extensions
+{
+	public class AdditionalUserClaimsPrincipalFactory
+			: UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+	{
+		public AdditionalUserClaimsPrincipalFactory(
+			UserManager<ApplicationUser> userManager,
+			RoleManager<IdentityRole> roleManager,
+			IOptions<IdentityOptions> optionsAccessor)
+			: base(userManager, roleManager, optionsAccessor)
+		{ }
+
+		public async override Task<ClaimsPrincipal> CreateAsync(ApplicationUser user)
+		{
+			var principal = await base.CreateAsync(user);
+			var identity = (ClaimsIdentity)principal.Identity;
+
+			var claims = new List<Claim>();
+			//if (user.IsAdmin)
+			//{
+			//	claims.Add(new Claim(JwtClaimTypes.Role, "admin"));
+			//}
+			//else
+			//{
+			//	claims.Add(new Claim(JwtClaimTypes.Role, "user"));
+			//}
+			if (!string.IsNullOrEmpty(user.FirstName))
+            {
+				claims.Add(new Claim("FirstName", user.FirstName));
+            }
+
+
+			identity.AddClaims(claims);
+			return principal;
+		}
+	}
+}
---
+++ IdentityDemo/Extensions/Email/AngusEmailSender.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Extensions.Email
+{
+    public class AngusEmailSender : IEmailSender
+    {
+        private readonly EmailSettings _emailSettings;
+
+        public AngusEmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string textMessage)
+        {
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress(_emailSettings.SenderName, _emailSettings.SenderEmail));
+            mimeMessage.To.Add(MailboxAddress.Parse(email));
+
+            mimeMessage.Subject = subject;
+            var builder = new BodyBuilder { HtmlBody = textMessage };
+            mimeMessage.Body = builder.ToMessageBody();
+
+            try
+            {
+                using var client = new SmtpClient();
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                if (_emailSettings.IsDevelopment)
+                    await client.ConnectAsync(_emailSettings.MailServer, _emailSettings.MailPort, _emailSettings.UseSsl)
+                        .ConfigureAwait(false);
+                else
+                    await client.ConnectAsync(_emailSettings.MailServer).ConfigureAwait(false);
+
+                // Note: only needed if the SMTP server requires authentication
+                await client.AuthenticateAsync(_emailSettings.SenderEmail, _emailSettings.Password).ConfigureAwait(false);
+                await client.SendAsync(mimeMessage).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
